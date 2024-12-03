a7b15d689db32a50eca1584e65901ebc5b2f9c82
connor gammill <connor.gammill@outlook.com>
2024-12-03 11:12:08 -0500

    Add project files.

---
+++ assignment5.2.3/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace assignment5._2._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			Console.WriteLine("Enter a number");
+			int x = Convert.ToInt32(Console.ReadLine());
+			RecursionNumbers(x);
+		}
+		static void RecursionNumbers(int n)
+		{
+			if (n > 0)
+			{
+				Console.WriteLine($"{n}");
+				RecursionNumbers(n - 1);
+			}
+		}
+    }
+}
